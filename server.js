7967e73e89d47df727d477d0af43365f30e208eb
Suneel <skinfotech156@gmail.com>
2026-05-09 00:31:19 +0530

    fixed backend mail

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ try {
   const transporter= nodemailer.createTransport({ service: "gmail", 
 auth: {
 user: "skinfotech156@gmail.com",
-pass: "shwi exof yhwb zxxo"
+pass: "qumo fdpc dxyl nfdt"
 }
 });
 let mailOptions = {
@@ -38,15 +38,29 @@ text: `
     Message: ${message}
    `
 };
-await transporter.sendMail(mailOptions);
-res.status(200).json({ success: true, message: "mail sent" });
+transporter.sendMail(mailOptions, (error, info) =>
+{
+if(error) {
+   console.log(error);
+   return res.status(500).json({
+     success: false,
+     message: "Error sending email"
+     });
+}
+
+return res.status(200).json({
+    success: true,
+    message: "Mail sent successfully"
+});
+});
+
 }
  catch (error) {
-console.error(error);
-res.status(500).json({ msg: "Error sending email" });
+ console.error(error);
+ res.status(500).json({ msg: "Error sending email" });
 }
 });
-const PORT = process.env.pORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {  
-    console.log("Server running on PORT 5000");
+    console.log('Server running on PORT ${PORT}');
 });
