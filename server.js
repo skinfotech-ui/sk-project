b37c2ce3d2d2b3e143389a23a0a2afe733bbb183
Suneel <skinfotech156@gmail.com>
2026-05-15 00:16:51 +0530

    hardcoded resend key

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { Resend } = require("resend");
 
 const app = express();
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_2KdY1Rfg_26t1HvbvjDHTMEC4NBVk6gPz");
 
 // middleware
 app.use(cors());
